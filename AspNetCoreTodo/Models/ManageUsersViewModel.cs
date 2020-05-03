d3d344d1a6b07ca43e63032802497daa4912ffdd
cdyang <nwpu409@gmail.com>
2020-05-03 11:39:04 +0800

    add ManageUser page

---
+++ AspNetCoreTodo/Models/ManageUsersViewModel.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace AspNetCoreTodo.Models
+{
+    public class ManageUsersViewModel
+    {
+        public IdentityUser[] Administrators { get; set; }
+
+        public IdentityUser[] Everyone { get; set; }
+    }
+}
