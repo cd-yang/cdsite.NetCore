8f8289a71e558fe09d4bb76580ecd7e0cb54d955
cdyang <nwpu409@gmail.com>
2020-05-17 11:44:40 +0800

    add model proj

---
+++ Model/MessageModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Model
+{
+    public class MessageModel<T>
+    {
+        public bool Success { get; set; }
+
+        public string Msg { get; set; }
+
+        public List<T> Data { get; set; }
+    }
+}
